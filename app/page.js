e10a46b7bdccd6cd0c241e9b82abe06687789186
chocolate716 <aisuke716@gmail.com>
2023-06-08 11:28:56 +0900

    site htmlの記述

--- app/page.js
+++ app/page.js
@@ -4,7 +4,15 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-    <p>あいすけ</p>
+    <title>課題研究</title>
+    <body>
+    <h1>愛知工業大学名電高等学校のしるし</h1>
+    <p>まず始めに</p>
+    <h2>愛工大名電高校　部活動紹介</h2>
+    </body>
+   
+    <p></p>
+
     </main>
   )
 }
