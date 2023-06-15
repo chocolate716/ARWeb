d2a42b4ab0712c670be33dc247b23e451bd13a08
chocolate716 <101511671+chocolate716@users.noreply.github.com>
2023-06-15 11:28:03 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,16 +4,28 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-    <title>課題研究</title>
-    <body>
-    <h1>愛知工業大学名電高等学校のしるし</h1>
-    <p>まず始めに</p>
-    <h2>愛工大名電高校　部活動紹介</h2>
+    <!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+    <title>ブログ用</title>
+    <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script> 
+    //外部ライブラリを読み込む場合
+　　<script src="../js/aframe.min.js"></script>
+　　//同じサーバーにアップロードする場合
+    <!-- 使用したライブラリ等の情報およびライセンスは次を参照 https://da.lib.shimane-u.ac.jp/virtual_tour/about.html#used -->
+  </head>
+  <body>
     
-    <h2>愛工大名電高校　フロアマップ</h2>
-    </body>
-   
-    <p></p>
+  <a-scene vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false" cursor="fuse: false; rayOrigin: mouse">
+	  
+    <a-sky src="../img/background/1F_outside.JPG" rotation="0 0 1" position="0 0 0"></a-sky>
+    <a-box color="#E3916E" rotation="0 0 0" position="0 0 -10" width="2" height="2" depth="2"></a-box>
+  </a-scene>
+
+  </body>
+</html>
+
 
     </main>
   )
